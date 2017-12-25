f8b8083ee470b06009a32ff98f412847e6d70941
panforrest <guoqianp@gmail.com>
2017-12-25 15:38:56 -0500

    signup page to prove create accurate data by rendering json

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+// Full Documentation - https://www.turbo360.co/docs
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+
+/*  This is the home route. It renders the index.mustache page from the views directory.
+	Data is rendered using the Mustache templating engine. For more
+	information, view here: https://mustache.github.io/#demo */
+router.post('/register', function(req, res){
+	res.json({
+		confirmation: 'success',
+		body: req.body
+	})
+})
+
+module.exports = router
