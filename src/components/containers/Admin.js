3b35aa78578fad8907ac387eb2e85adc957afaef
panforrest <guoqianp@gmail.com>
2017-12-25 19:56:52 -0500

    1:01:00 my attempt to console log current user by Admin.js component

--- src/components/containers/Admin.js
+++ src/components/containers/Admin.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react'
+import superagent from 'superagent'
 
 class Admin extends Component {
+    componentDidMount(){
+    	// console.log('componentDidMount:')
+    	//who is logged in here???
+        superagent.get('/auth/currentuser')
+        .query(null)
+        .set('Accept', 'application/json')
+        .end((err, response) => {
+            if (err) {
+                console.log(err.message)
+                return
+            }
+
+            const data = response.body  //const data = response.body.data
+            console.log('componentDidMount: '+JSON.stringify(data))
+
+            // this.setState({
+            // 	jobs: data
+            // })
+        })
+
+    }
+
 	render(){
 		return(
 			<div>
